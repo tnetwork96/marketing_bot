59e56c5218f0b84b078a94997017e5eaaae7a4f7
tnetw <tnetwork96@gmail.com>
2024-04-01 22:24:35 +0700

    update

--- create_container_from_google_sheet.py
+++ create_container_from_google_sheet.py
@@ -9,7 +9,7 @@ cookies = gg_sheet_handler.read_all(cookies_spreadsheet_name)
 fb_users = [x for x in cookies[0] if x.strip()]
 print("--args: version - example: v1.0.0")
 """sudo docker run -itd --name test --network bridge tnetwork96/marketing_service_rpi_armv7:{1} ./script.sh {2}"""
-docker_run_command = "sudo docker run -itd --name {0} -v $(pwd):/home/marketing-bot --network bridge tnetwork96/marketing_service_rpi_armv7:{1} ./script.sh {2} "
+docker_run_command = "sudo docker run -itd --name {0} -v $(pwd):/home/marketing-bot --network bridge tnetwork96/marketing_bot_armv8:{1} ./script.sh {2} "
 client = docker.from_env()
 client_list = client.containers.list(all=True)
 container_names = [cli.name for cli in client_list
